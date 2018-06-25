e54950b87ac10e62f21a8373b113ccf586306904
arsalon <arsalon47@gmail.com>
2018-06-25 16:24:27 -0400

    2nd drill done

--- hashmap.js
+++ hashmap.js
@@ -129,22 +129,22 @@ function anagram(arr) {
   return result;
 }
 
-function main() {
-  // let lor = new HashMap();
-  // lor.set('Hobbit', 'Bilbo');
-  // lor.set('Hobbit', 'Frodo');
-  // lor.set('Wizard', 'Gandolf');
-  // lor.set('Human', 'Aragon');
-  // lor.set('Elf', 'Legolas');
-  // lor.set('Maiar', 'The Necromancer');
-  // lor.set('Maiar', 'Sauron');
-  // lor.set('RingBearer', 'Gollum');
-  // lor.set('LadyOfLight', 'Galadriel');
-  // lor.set('HalfEleven', 'Arwen');
-  // lor.set('Ent', 'Treebeard');
-  // console.log(lor);
-  // console.log(lor.get('Maiar'));
-  // console.log(palindrome('rraacce'))
-  console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']))
-}
-main();
+// function main() {
+//   // let lor = new HashMap();
+//   // lor.set('Hobbit', 'Bilbo');
+//   // lor.set('Hobbit', 'Frodo');
+//   // lor.set('Wizard', 'Gandolf');
+//   // lor.set('Human', 'Aragon');
+//   // lor.set('Elf', 'Legolas');
+//   // lor.set('Maiar', 'The Necromancer');
+//   // lor.set('Maiar', 'Sauron');
+//   // lor.set('RingBearer', 'Gollum');
+//   // lor.set('LadyOfLight', 'Galadriel');
+//   // lor.set('HalfEleven', 'Arwen');
+//   // lor.set('Ent', 'Treebeard');
+//   // console.log(lor);
+//   // console.log(lor.get('Maiar'));
+//   // console.log(palindrome('rraacce'))
+//   console.log(anagram(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']))
+// }
+// main();
---
+++ separate-chaining.js
@@ -0,0 +1,186 @@
+class _Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    }
+    else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+  find(item) {
+    //start at the head
+    let currNode = this.head;
+    //if the list is empty
+    if (!this.head) {
+      return null;
+    }
+    //Check for the item 
+    while (currNode.value !== item) {
+      //return null if end of the list 
+      // and the item is not on the list
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        //otherwise keep looking 
+        currNode = currNode.next;
+      }
+    }
+    //found it
+    return currNode;
+  }
+
+  remove(item) {
+    //if the list is empty
+    if (!this.head) {
+      return null;
+    }
+    //if the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    //start at the head
+    let currNode = this.head;
+    //keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      //save the previous node 
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
+}
+
+class HashMap {
+  constructor(initialCapacity = 8) {
+    this.length = 0;
+    this._slots = [];
+    this._capacity = initialCapacity;
+    this._deleted = 0;
+  }
+
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._slots[index] === undefined) {
+      throw new Error('Key error');
+    }
+    return this._slots[index].value;
+  }
+
+  set(key, value) {
+    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMap.SIZE_RATIO);
+    }
+
+    const index = this._findSlot(key);
+    if (!this._slots[index]) {
+      this.length++;
+    }
+    this._slots[index] = {
+      key,
+      value,
+      deleted: false
+    };
+  }
+
+  remove(key) {
+    const index = this._findSlot(key);
+    const slot = this._slots[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.deleted = true;
+    this.length--;
+    this._deleted++;
+  }
+
+  _findSlot(key) {
+    const hash = HashMap._hashString(key);
+    const start = hash % this._capacity;
+
+    for (let i = start; i < start + this._capacity; i++) {
+      const index = i % this._capacity;
+      const slot = this._slots[index];
+      if (slot === undefined || (slot.key === key && !slot.deleted)) {
+        return index;
+      }
+    }
+  }
+
+  _resize(size) {
+    const oldSlots = this._slots;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items back
+    this.length = 0;
+    this._deleted = 0;
+    this._slots = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.deleted) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
+  }
+}
+
+HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.SIZE_RATIO = 3;
+
+// check to see if something exists at an index
+// if index is empty put key/value pair in
+// if index is occupied attach current key/value pair to the end of
+//  the linked list at the index
+
+function main() {
+  let lor = new HashMap();
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfEleven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  console.log(JSON.stringify(lor, null, 2));
+}
+main();
